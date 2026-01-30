634e7bde103e664f9a8eabd1bcad8c10b6df6479
arya27Q <ayrandrapratama@gmail.com>
2026-01-30 08:12:49 +0700

    p

--- database/migrations/tenant/2026_01_07_033949_create_test_accounts_table.php
+++ database/migrations/tenant/2026_01_07_033949_create_test_accounts_table.php
@@ -11,8 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // --- TAMBAHAN: Cek dulu apakah tabel 'akun' sudah ada ---
-        // Kalau tabel belum ada, baru dibuat. Kalau sudah ada, dilewati.
+       
         if (!Schema::hasTable('akun')) {
             
             Schema::create('akun', function (Blueprint $table) {
@@ -20,7 +19,6 @@ return new class extends Migration
                 $table->string('name');
                 $table->string('email')->unique(); 
                 $table->string('password');
-                // pt_name dihapus karena akun sudah berada di dalam database tenant masing-masing
                 $table->timestamps();
             });
             
